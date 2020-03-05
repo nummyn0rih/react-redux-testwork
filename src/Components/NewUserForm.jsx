c9f097e4e31de24761619d0d25a00cb841115922
nummyn0rih <nummyn0rih@gmail.com>
2020-03-05 21:38:16 +0300

    refactoring NewUserForm

--- src/Components/NewUserForm.jsx
+++ src/Components/NewUserForm.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { reduxForm, Field, SubmissionError } from 'redux-form';
-import uniqueId from 'lodash.uniqueid';
 import Button from './Button';
 import * as actions from '../actions';
 
@@ -9,8 +8,8 @@ const mapStateToProps = (state) => {
   const { users: { allIds }, uiState: { newUserForm } } = state;
   const fields = [
     'id',
-    'first name',
-    'last name',
+    'firstName',
+    'lastName',
     'email',
     'phone',
   ];
@@ -45,8 +44,33 @@ class NewUserForm extends React.Component {
     hideNewUserForm();
   }
 
+  renderFields = () => {
+    const { fields } = this.props;
+
+    return fields.map((fieldName) => (
+      <div className="mdc-text-field__wrapper" key={fieldName}>
+        <label className="mdc-floating-label" htmlFor={fieldName}>{fieldName}</label>
+        <div className="mdc-text-field mdc-text-field--outlined">
+          <Field
+            className="mdc-text-field__input"
+            name={fieldName}
+            id={fieldName}
+            component="input"
+            type={fieldName === "id" || "phone" ? "number" : "text"}  
+            required
+          />
+          <div className="mdc-notched-outline">
+            <div className="mdc-notched-outline__leading"></div>
+            <div className="mdc-notched-outline__notch"></div>
+            <div className="mdc-notched-outline__trailing"></div>
+          </div>
+        </div>
+      </div>
+    ));
+  }
+
   render() {
-    const { newUserForm, handleSubmit, fields } = this.props;
+    const { newUserForm, handleSubmit } = this.props;
 
     if (newUserForm === 'hide') {
       return null;
@@ -54,100 +78,10 @@ class NewUserForm extends React.Component {
 
     return (
       <form onSubmit={handleSubmit(this.submit)} className="mdc-form-field">
-
-        <div className="">
-          <label className="" htmlFor="id">ID</label>
-          <div className="mdc-text-field mdc-text-field--outlined">
-            <Field
-              className="mdc-text-field__input"
-              name="id"
-              id="id"
-              component="input"
-              type="number"
-              required
-            />
-            <div className="mdc-notched-outline">
-              <div className="mdc-notched-outline__leading"></div>
-              <div className="mdc-notched-outline__notch"></div>
-              <div className="mdc-notched-outline__trailing"></div>
-            </div>
-          </div>
-        </div>
-        <div className="">
-          <label className="" htmlFor="first-name">First name</label>
-          <div className="mdc-text-field mdc-text-field--outlined">
-            <Field
-              className="mdc-text-field__input"
-              name="firstName"
-              id="first-name"
-              component="input"
-              type="text"
-              
-            />
-            <div className="mdc-notched-outline">
-              <div className="mdc-notched-outline__leading"></div>
-              <div className="mdc-notched-outline__notch"></div>
-              <div className="mdc-notched-outline__trailing"></div>
-            </div>
-          </div>
-        </div><div className="">
-          <label className="" htmlFor="last-name">Last name</label>
-          <div className="mdc-text-field mdc-text-field--outlined">
-            <Field
-              className="mdc-text-field__input"
-              name="lastName"
-              id="last-name"
-              component="input"
-              type="text"
-              
-            />
-            <div className="mdc-notched-outline">
-              <div className="mdc-notched-outline__leading"></div>
-              <div className="mdc-notched-outline__notch"></div>
-              <div className="mdc-notched-outline__trailing"></div>
-            </div>
-          </div>
-        </div>
-        <div className="">
-          <label className="" htmlFor="email">Email</label>
-          <div className="mdc-text-field mdc-text-field--outlined">
-            <Field
-              className="mdc-text-field__input"
-              name="email"
-              id="email"
-              component="input"
-              type="email"
-              
-            />
-            <div className="mdc-notched-outline">
-              <div className="mdc-notched-outline__leading"></div>
-              <div className="mdc-notched-outline__notch"></div>
-              <div className="mdc-notched-outline__trailing"></div>
-            </div>
-          </div>
-        </div>
-        <div className="">
-          <label className="" htmlFor="phone">Phone</label>
-          <div className="mdc-text-field mdc-text-field--outlined">
-            <Field
-              className="mdc-text-field__input"
-              name="phone"
-              id="phone"
-              component="input"
-              type="tel"
-              
-            />
-            <div className="mdc-notched-outline">
-              <div className="mdc-notched-outline__leading"></div>
-              <div className="mdc-notched-outline__notch"></div>
-              <div className="mdc-notched-outline__trailing"></div>
-            </div>
-          </div>
-        </div>
-
-        <div className="wrapper__new-user-form_btn">
-          <Button handleOnClick={this.handleHideNewUserForm} text="Close" />
-          <Button text="Add" />
+        {this.renderFields()}
+        <div className="new-user-form-btn__wrapper">
+          <Button handleOnClick={this.handleHideNewUserForm} text="X" />
+          <Button text="Add" raised={true} />
         </div>
       </form>
     );
